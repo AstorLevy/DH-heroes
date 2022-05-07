626e677628a0a5db44eeeb90d8b68db3d4b3b0bd
Astor Levy <102769883+AstorLevy@users.noreply.github.com>
2022-05-07 13:35:37 -0300

    add: archivos principales

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+
+// Routes
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/index.html"));
+})
+app.get("/babbage", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/babbage.html"));
+    
+})
+app.get("/berners-lee", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/berners-lee.html"));
+    
+})
+app.get("/clarke", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/clarke.html"));
+    
+})
+app.get("/hamilton", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/hamilton.html"));
+    
+})
+app.get("/hopper", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/hopper.html"));
+    
+})
+app.get("/lovelace", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/lovelace.html"));
+    
+})
+app.get("/turing", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/turing.html"));
+
+})
+
+// Static
+app.use(express.static(path.join(__dirname, "public")));
+
+// Server
+app.listen(3030, () => {
+    console.log("Servidor corriendo en puerto 3030");
+})
